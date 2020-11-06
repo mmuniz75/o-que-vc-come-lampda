0649aec457385f0f361e370380885a718677f7d3
mmuniz75 <mmunizs1975@gmail.com>
2020-11-06 07:29:39 -0300

    iniciando classes model

---
+++ model/__init__.py
@@ -0,0 +1,5 @@
+from model.BrandFoodModel import BrandFoodModel
+from model.FoodModel import FoodModel
+from model.BrandModel import BrandModel
+from model.ChemicalModel import ChemicalModel
+from model.BrandFoodChemicalModel import BrandFoodChemicalModel
